1df97af48237d7ed567a1f315f92c79c87eb652c
nabillahsujaen <nabillahbella49@gmail.com>
2021-10-18 22:03:51 +0700

    Tugas Pertemuan 5

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -3,8 +3,8 @@ class Contoh1 extends CI_Controller{
     public function index()
     {
         echo "<h1>Perkenalkan</h1><br>";
-        echo "Nama Saya Robih <br>
-              Saya tinggal di Kp.sukapura Jaya<br>
-              olahraga yang saya sukai sepak bola & futsal";
+        echo "Nama Saya nabilah sujaen <br>
+              Saya tinggal di bekasi<br>
+              olahraga shopping";
     }
 }
